7fa7045f3cb90128dab19f8535b1cfbfeba5f56b
marquisalexander <marquissantos123@gmail.com>
2021-06-07 20:30:52 -0300

    criando estilos globais

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import { Container, Title } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
