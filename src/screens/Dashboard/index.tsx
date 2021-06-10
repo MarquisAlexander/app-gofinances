e364825a1a1313da3b05c7199f3ec012df953cc7
marquisalexander <marquissantos123@gmail.com>
2021-06-10 20:50:08 -0300

    utilizando icones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -9,6 +9,7 @@ import {
   User,
   UserGreeting,
   UserName,
+  Icon,
 } from "./styles";
 
 export function Dashboard() {
@@ -27,6 +28,7 @@ export function Dashboard() {
               <UserName>Marquis</UserName>
             </User>
           </UserInfo>
+          <Icon name="power" />
         </UserWrapper>
       </Header>
     </Container>
